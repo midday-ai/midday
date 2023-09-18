131ed3113d9195658adccf84d95109de3ec5ebbb
Pontus Abrahamsson <pontus@lostisland.co>
2023-09-18 20:53:51 +0200

    first commit

--- apps/site/src/app/[locale]/page.tsx
+++ apps/site/src/app/[locale]/page.tsx
@@ -1,10 +1,8 @@
- export default async function Home() {
+export default async function Home() {
   return (
     <div className="mx-auto w-full max-w-xl">
-      <div className="flex justify-between py-6">
-      </div>
-      <div>
-      </div>
+      <div className="flex justify-between py-6"></div>
+      <div></div>
     </div>
   );
 }
--- apps/site/src/app/layout.tsx
+++ apps/site/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+
 import type { ReactElement } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
