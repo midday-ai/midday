724ac73dc9cbdc64587d96d5c52a5bcdf0d4a9ea
Pontus Abrahamsson <pontus@lostisland.co>
2023-10-05 23:23:11 +0200

    subscribe

--- apps/site/src/app/layout.tsx
+++ apps/site/src/app/layout.tsx
@@ -2,13 +2,13 @@ import "@/styles/globals.css";
 import { cn } from "@midday/ui";
 
 import type { Metadata } from "next";
-// import { Plus_Jakarta_Sans } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import type { ReactElement } from "react";
 
-// const fontSans = Plus_Jakarta_Sans({
-//   subsets: ["latin"],
-//   variable: "--font-sans",
-// });
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Midday | Smart pre-accounting",
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: ReactElement }) {
   return (
     <html lang="en">
-      <body className={cn("bg-background")}>{children}</body>
+      <body className={cn(fontSans.variable, "bg-background")}>{children}</body>
     </html>
   );
 }
