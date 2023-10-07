721c64f96f6931d5be20d64dd4b43b08f4e640ff
Pontus Abrahamsson <pontus@lostisland.co>
2023-10-07 15:27:17 +0200

    ui wip

--- apps/app/src/components/google-sign-in.tsx
+++ apps/app/src/components/google-sign-in.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { createClientComponentClient } from "@midday/supabase";
+import { Button } from "@midday/ui/button";
 
 export function GoogleSignIn() {
 	const supabase = createClientComponentClient();
@@ -15,12 +16,11 @@ export function GoogleSignIn() {
 	};
 
 	return (
-		<button
-			type="button"
+		<Button
 			onClick={handleSignIn}
-			className="appearance-none font-semibold scale-100 active:scale-[0.98] flex w-full items-center justify-center rounded-xl bg-gray-800 px-10 py-4 text-gray-100 transition-colors duration-150 hover:bg-gray-700 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-gray-200"
+			className="active:scale-[0.98] rounded-xl bg-white px-10 py-4 text-black"
 		>
 			Continue with Google
-		</button>
+		</Button>
 	);
 }
