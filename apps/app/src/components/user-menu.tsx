2070845a316097ff24536502973b7ff52eb64209
Pontus Abrahamsson <pontus@lostisland.co>
2023-10-07 13:32:46 +0200

    Icons

--- apps/app/src/components/user-menu.tsx
+++ apps/app/src/components/user-menu.tsx
@@ -8,7 +8,7 @@ export async function UserMenu() {
 	return (
 		<div className="px-4 py-6 flex space-x-2">
 			<Image
-				src={user.avatar_url}
+				src={user?.avatar_url}
 				width={24}
 				height={24}
 				className="rounded-full"
