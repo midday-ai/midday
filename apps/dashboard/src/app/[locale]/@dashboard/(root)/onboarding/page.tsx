cf4a86ab1694992a472d136982fa262c1bc4a4b4
Pontus Abrahamsson <pontus@lostisland.co>
2023-10-08 19:34:19 +0200

    Onboarding

--- apps/dashboard/src/app/[locale]/@dashboard/(root)/onboarding/page.tsx
+++ apps/dashboard/src/app/[locale]/@dashboard/(root)/onboarding/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Onboarding | Midday",
+};
+
 export default function Onboarding() {
 	return null;
 }
--- apps/dashboard/src/app/[locale]/@dashboard/(root)/page.tsx
+++ apps/dashboard/src/app/[locale]/@dashboard/(root)/page.tsx
@@ -1,9 +1,16 @@
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
 	title: "Overview | Midday",
 };
 
-export default function Overview() {
-	return <p>wdfwef</p>;
+export default async function Overview() {
+	const connected = false;
+
+	if (!connected) {
+		redirect("/onboarding");
+	}
+
+	return <p>Overview</p>;
 }
