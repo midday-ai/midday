4a9dbd56b625c214fbf6658c9644c576e27a26a6
Pontus Abrahamsson <pontus@lostisland.co>
2023-10-09 17:09:29 +0200

    Command menu

---
+++ apps/dashboard/src/components/command-menu.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import { Button } from "@midday/ui/button";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@midday/ui/command";
+import { cn } from "@midday/ui/utils";
+import { DialogProps } from "@radix-ui/react-alert-dialog";
+import { LaptopIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useRouter } from "next/navigation";
+import * as React from "react";
+
+export function CommandMenu({ ...props }: DialogProps) {
+  const router = useRouter();
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  const runCommand = React.useCallback((command: () => unknown) => {
+    setOpen(false);
+    command();
+  }, []);
+
+  return (
+    <>
+      <Button
+        variant="outline"
+        className={cn(
+          "relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64",
+        )}
+        onClick={() => setOpen(true)}
+        {...props}
+      >
+        <span className="hidden lg:inline-flex">Search for or jump to</span>
+        <span className="inline-flex lg:hidden">Search...</span>
+        <kbd className="pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          {/* <CommandGroup heading="Links">
+            {docsConfig.mainNav
+              .filter((navitem) => !navitem.external)
+              .map((navItem) => (
+                <CommandItem
+                  key={navItem.href}
+                  value={navItem.title}
+                  onSelect={() => {
+                    runCommand(() => router.push(navItem.href as string));
+                  }}
+                >
+                  <FileIcon className="mr-2 h-4 w-4" />
+                  {navItem.title}
+                </CommandItem>
+              ))}
+          </CommandGroup> */}
+          {/* {docsConfig.sidebarNav.map((group) => (
+            <CommandGroup key={group.title} heading={group.title}>
+              {group.items.map((navItem) => (
+                <CommandItem
+                  key={navItem.href}
+                  value={navItem.title}
+                  onSelect={() => {
+                    runCommand(() => router.push(navItem.href as string));
+                  }}
+                >
+                  <div className="mr-2 flex h-4 w-4 items-center justify-center">
+                    <CircleIcon className="h-3 w-3" />
+                  </div>
+                  {navItem.title}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ))} */}
+          <CommandSeparator />
+          <CommandGroup heading="Theme">
+            <CommandItem
+              onSelect={() => runCommand(() => console.log("light"))}
+            >
+              <SunIcon className="mr-2 h-4 w-4" />
+              Light
+            </CommandItem>
+            <CommandItem onSelect={() => runCommand(() => console.log("dark"))}>
+              <MoonIcon className="mr-2 h-4 w-4" />
+              Dark
+            </CommandItem>
+            <CommandItem
+              onSelect={() => runCommand(() => console.log("system"))}
+            >
+              <LaptopIcon className="mr-2 h-4 w-4" />
+              System
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+}
--- apps/dashboard/src/components/header.tsx
+++ apps/dashboard/src/components/header.tsx
@@ -1,12 +1,12 @@
+import { CommandMenu } from "@/components/command-menu";
 import { Feedback } from "@/components/feedback";
 import { Notifications } from "@/components/notifications";
-import { Search } from "@/components/search";
 import { UserMenu } from "@/components/user-menu";
 
 export function Header() {
   return (
     <header className="border-b-[1px] flex justify-between py-4 mr-8">
-      <Search />
+      <CommandMenu />
       <div className="flex space-x-2">
         <Feedback />
         <Notifications />
