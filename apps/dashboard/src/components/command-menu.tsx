e6c712db2104ec9663b1bf0630f3efd8824b987c
Pontus Abrahamsson <pontus@lostisland.co>
2023-10-09 17:20:17 +0200

    Command menu

--- apps/dashboard/src/components/command-menu.tsx
+++ apps/dashboard/src/components/command-menu.tsx
@@ -10,7 +10,6 @@ import {
   CommandList,
   CommandSeparator,
 } from "@midday/ui/command";
-import { cn } from "@midday/ui/utils";
 import { DialogProps } from "@radix-ui/react-alert-dialog";
 import { LaptopIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useRouter } from "next/navigation";
@@ -41,14 +40,11 @@ export function CommandMenu({ ...props }: DialogProps) {
     <>
       <Button
         variant="outline"
-        className={cn(
-          "relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64",
-        )}
+        className="relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64 border-0 p-0 hover:bg-transparent font-normal"
         onClick={() => setOpen(true)}
         {...props}
       >
         <span className="hidden lg:inline-flex">Search for or jump to</span>
-        <span className="inline-flex lg:hidden">Search...</span>
         <kbd className="pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
           <span className="text-xs">⌘</span>K
         </kbd>
--- apps/dashboard/src/components/feedback.tsx
+++ apps/dashboard/src/components/feedback.tsx
@@ -2,7 +2,10 @@ import { Button } from "@midday/ui/button";
 
 export function Feedback() {
   return (
-    <Button variant="outline" className="rounded-lg font-normal h-8 py-0 px-4">
+    <Button
+      variant="outline"
+      className="rounded-lg font-normal h-8 py-0 px-4 text-sm text-muted-foreground"
+    >
       Feedback
     </Button>
   );
