84cec60fc76ce4bdaf3ca77ca44c70c6071045fc
Pontus Abrahamsson <pontus@lostisland.co>
2023-10-28 14:24:17 +0200

    Auth refresh

--- apps/dashboard/src/actions/vote-action.ts
+++ apps/dashboard/src/actions/vote-action.ts
@@ -3,13 +3,12 @@
 import { createClient } from "@vercel/kv";
 import { revalidatePath } from "next/cache";
 
+const kv = createClient({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
+});
+
 export const voteAction = async (id: string, path: string) => {
-  const kv = createClient({
-    url: process.env.KV_REST_API_URL,
-    token: process.env.KV_REST_API_TOKEN,
-  });
-
   await kv.incr(id);
-
   revalidatePath(path);
 };
