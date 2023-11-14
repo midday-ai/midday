d78125d881cb10b6e89783f8f786fd425b4e1402
Pontus Abrahamsson <pontus@lostisland.co>
2023-11-14 11:27:30 +0100

    wip

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  transpilePackages: ["@midday/ui", "@midday/tailwind"],
+  transpilePackages: ["@midday/ui", "@midday/tailwind", "@midday/location"],
   experimental: {
     ppr: true,
   },
