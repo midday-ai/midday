30b9f9a8da9aa3bd68142f4ff6ed7fee3ae16eaa
Pontus Abrahamsson <pontus@lostisland.co>
2023-11-30 13:07:11 +0100

    robots

--- apps/dashboard/src/components/tables/vault/empty-table.tsx
+++ apps/dashboard/src/components/tables/vault/empty-table.tsx
@@ -17,6 +17,33 @@ export function EmptyTable({ type }: Props) {
         </div>
       );
 
+    case "exports":
+      return (
+        <div className="h-[calc(100%-80px)] p-4 flex justify-center items-center">
+          <div className="items-center flex flex-col text-center">
+            <h2 className="mb-2">Exports</h2>
+            <p className="text-sm text-[#878787]">
+              This is where your exports based from <br />
+              transactions will end up.
+            </p>
+          </div>
+        </div>
+      );
+
+    case "transactions":
+      return (
+        <div className="h-[calc(100%-80px)] p-4 flex justify-center items-center">
+          <div className="items-center flex flex-col text-center">
+            <h2 className="mb-2">Transactions</h2>
+            <p className="text-sm text-[#878787]">
+              This is where your attachments for
+              <br />
+              transactions will end up.
+            </p>
+          </div>
+        </div>
+      );
+
     default:
       return (
         <div className="h-[calc(100%-80px)] p-4 flex justify-center items-center">
