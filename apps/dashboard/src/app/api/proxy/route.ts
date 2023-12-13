62c48abb81d7b038e04a6a85aae9c4ffc74075ef
Pontus Abrahamsson <pontus@lostisland.co>
2023-12-13 19:22:23 +0100

    Proxy

--- apps/dashboard/src/app/api/proxy/route.ts
+++ apps/dashboard/src/app/api/proxy/route.ts
@@ -4,15 +4,10 @@ export async function GET(req) {
   const requestUrl = new URL(req.url);
   const filePath = requestUrl.searchParams.get("filePath");
 
-  console.log(
-    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/${filePath}`
-  );
-
   return fetch(
     `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/${filePath}`,
     {
       headers: {
-        // TODO: Use current user token instead
         authorization: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       },
     }
