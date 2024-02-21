da10962762fdd0a587aa4803e405fdd757026187
Pontus Abrahamsson <pontus@lostisland.co>
2024-02-21 15:37:20 +0100

    Fix command menu

--- apps/dashboard/src/components/command-menu/ai.tsx
+++ apps/dashboard/src/components/command-menu/ai.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import { BackButton } from "@/components/command-menu/back-button";
+import { MenuOption, useCommandStore } from "@/store/command";
+import { Button } from "@midday/ui/button";
 
 export function CommandAI() {
+  const { setMenu } = useCommandStore();
+
   return (
     <div className="h-full">
       <div className="p-5 flex items-center space-x-3">
@@ -8,8 +14,11 @@ export function CommandAI() {
         <h2>Midday AI</h2>
       </div>
 
-      <div className="mt-28 flex items-center justify-center text-sm">
-        <p>Under development</p>
+      <div className="mt-24 flex items-center justify-center text-sm flex-col space-y-4">
+        <p>Not enabled for this account yet.</p>
+        <Button variant="outline" onClick={() => setMenu(MenuOption.Feedback)}>
+          Request access
+        </Button>
       </div>
     </div>
   );
