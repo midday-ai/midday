7469f57ef71a73f6d88858c139102f1213a0ff6d
Pontus Abrahamsson <pontus@lostisland.co>
2024-02-25 10:49:54 +0100

    Add insights

--- apps/dashboard/src/actions/connect-bank-account-action.ts
+++ apps/dashboard/src/actions/connect-bank-account-action.ts
@@ -76,6 +76,7 @@ export const connectBankAccountAction = action(
     revalidateTag(`spending_${teamId}`);
     revalidateTag(`metrics_${teamId}`);
     revalidateTag(`bank_accounts_${teamId}`);
+    revalidateTag(`insights_${teamId}`);
 
     logsnag.track({
       event: LogEvents.ConnectBankCompleted.name,
