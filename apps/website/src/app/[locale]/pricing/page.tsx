888d5277e00a2e39b979e203ad042f8ca71da856
Pontus Abrahamsson <pontus@lostisland.co>
2024-03-16 16:57:05 +0100

    Pricing page

--- apps/website/src/app/[locale]/pricing/page.tsx
+++ apps/website/src/app/[locale]/pricing/page.tsx
@@ -1,4 +1,6 @@
+import { BlurryCircle } from "@/components/blurry-circle";
 import { CTAButtons } from "@/components/cta-buttons";
+import { Testimonials } from "@/components/testimonials";
 import { getStaticParams } from "@/locales/server";
 import {
   Accordion,
@@ -26,95 +28,104 @@ export default function Page({
   setStaticParamsLocale(locale);
 
   return (
-    <div className="container max-w-[800px]">
-      <div className="h-screen">
-        <h1 className="mt-24 font-medium text-center text-5xl mb-8">
-          What it cost.
-        </h1>
+    <>
+      <div className="container max-w-[800px]">
+        <div className="h-screen">
+          <h1 className="mt-24 font-medium text-center text-5xl mb-8">
+            What it cost.
+          </h1>
 
-        <div className="flex items-center flex-col text-center">
-          <h3 className="text-[344px] font-medium leading-[344px]">30</h3>
+          <div className="flex items-center flex-col text-center relative">
+            <h3 className="text-[344px] font-medium leading-[344px]">30</h3>
 
-          <p className="font-medium text-xl">
-            Claim $30/mo early adopter plan, <br />
+            <p className="font-medium text-xl">
+              Claim $30/mo early adopter plan
+            </p>
+
             <span className="underline">free while in beta</span>
-          </p>
 
-          <div className="mt-6">
-            <CTAButtons />
+            <div className="mt-6">
+              <CTAButtons />
+            </div>
+
+            <BlurryCircle className="absolute -top-[50px] right-[0px] bg-[#3633D0]/5" />
+            <BlurryCircle className="absolute bottom-[160px] left-6 bg-[#A1F5CD]/5" />
+            <BlurryCircle className="absolute bottom-0 right-[150px] bg-[#FFECBB]/5" />
           </div>
         </div>
-      </div>
 
-      <div className="-mt-[150px]">
-        <div className="text-center">
-          <h4 className="text-4xl">Frequently asked questions</h4>
-          <p className="text-[#878787] text-sm mt-4">
-            Integer quis vestibulum lorem. Curabitur consectetur nulla nec justo
-            <br />
-            congue mattis. Nulla tincidunt ante eros, nec interdum dui varius
-            quis.
-          </p>
-        </div>
+        <div className="-mt-[150px]">
+          <div className="text-center">
+            <h4 className="text-4xl">Frequently asked questions</h4>
+            <p className="text-[#878787] text-sm mt-4">
+              Integer quis vestibulum lorem. Curabitur consectetur nulla nec
+              justo
+              <br />
+              congue mattis. Nulla tincidunt ante eros, nec interdum dui varius
+              quis.
+            </p>
+          </div>
 
-        <Accordion type="single" collapsible className="w-full mt-10 mb-48">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Can I self-host Midday.ai?</AccordionTrigger>
-            <AccordionContent>
-              Absolutely. Please refer to our docs for the latest guides and
-              information on self-hosting the platform.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger>Can I run Midday.ai locally?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It comes with default styles that matches the other
-              components&apos; aesthetic.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger>Is Midday.ai open source?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It's animated by default, but you can disable it if you
-              prefer.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-4">
-            <AccordionTrigger>
-              What are your data privacy & security policies?
-            </AccordionTrigger>
-            <AccordionContent>
-              We take data privacy very seriously and implement state-of-the-art
-              security measures to protect your data. We are also actively
-              working towards SOC 2 Type II compliance. You can learn more about
-              our data privacy policies at{" "}
-              <Link href="/privacy">midday.co/privacy</Link>.
-            </AccordionContent>
-            <AccordionItem value="item-5">
-              <AccordionTrigger>
-                Can I cancel my subscription at any time?
-              </AccordionTrigger>
+          <Accordion type="single" collapsible className="w-full mt-10 mb-48">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Can I self-host Midday.ai?</AccordionTrigger>
               <AccordionContent>
-                Yes, you can cancel your subscription at any time. If you cancel
-                your subscription, you will still be able to use Midday until
-                the end of your billing period. After that, you will be
-                downgraded to the free plan.
+                Absolutely. Please refer to our docs for the latest guides and
+                information on self-hosting the platform.
               </AccordionContent>
             </AccordionItem>
-          </AccordionItem>
-          <AccordionItem value="item-6">
-            <AccordionTrigger>
-              I have more questions about Midday.ai. How can I get in touch?
-            </AccordionTrigger>
-            <AccordionContent>
-              Sure, we're happy to answer any questions you might have. Just
-              send us an email at{" "}
-              <a href="mailto:support@midday.ai">support@midday.ai</a> and we'll
-              get back to you as soon as possible.
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+            <AccordionItem value="item-2">
+              <AccordionTrigger>Can I run Midday.ai locally?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It comes with default styles that matches the other
+                components&apos; aesthetic.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger>Is Midday.ai open source?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It's animated by default, but you can disable it if you
+                prefer.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4">
+              <AccordionTrigger>
+                What are your data privacy & security policies?
+              </AccordionTrigger>
+              <AccordionContent>
+                We take data privacy very seriously and implement
+                state-of-the-art security measures to protect your data. We are
+                also actively working towards SOC 2 Type II compliance. You can
+                learn more about our data privacy policies at{" "}
+                <Link href="/privacy">midday.co/privacy</Link>.
+              </AccordionContent>
+              <AccordionItem value="item-5">
+                <AccordionTrigger>
+                  Can I cancel my subscription at any time?
+                </AccordionTrigger>
+                <AccordionContent>
+                  Yes, you can cancel your subscription at any time. If you
+                  cancel your subscription, you will still be able to use Midday
+                  until the end of your billing period. After that, you will be
+                  downgraded to the free plan.
+                </AccordionContent>
+              </AccordionItem>
+            </AccordionItem>
+            <AccordionItem value="item-6">
+              <AccordionTrigger>
+                I have more questions about Midday.ai. How can I get in touch?
+              </AccordionTrigger>
+              <AccordionContent>
+                Sure, we're happy to answer any questions you might have. Just
+                send us an email at{" "}
+                <a href="mailto:support@midday.ai">support@midday.ai</a> and
+                we'll get back to you as soon as possible.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </div>
-    </div>
+      <Testimonials />
+    </>
   );
 }
--- apps/website/src/components/startpage.tsx
+++ apps/website/src/components/startpage.tsx
@@ -1,13 +1,13 @@
 import { Hero } from "@/components/hero";
 import { Screens } from "@/components/screens";
+import { SectionFive } from "@/components/section-five";
+import { SectionFour } from "@/components/section-four";
 import { SectionOne } from "@/components/section-one";
+import { SectionSeven } from "@/components/section-seven";
+import { SectionSix } from "@/components/section-six";
+import { SectionThree } from "@/components/section-three";
 import { SectionTwo } from "@/components/section-two";
-import { SectionFive } from "./section-five";
-import { SectionFour } from "./section-four";
-import { SectionSeven } from "./section-seven";
-import { SectionSix } from "./section-six";
-import { SectionThree } from "./section-three";
-import { Testimonials } from "./testimonials";
+import { Testimonials } from "@/components/testimonials";
 
 export function StartPage() {
   return (
