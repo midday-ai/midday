5173cdca577d8269f9d1a4fb0cbb291c219d7b0c
Pontus Abrahamsson <pontus@lostisland.co>
2024-03-16 15:02:06 +0100

    testimonials

--- apps/website/src/components/testimonials.tsx
+++ apps/website/src/components/testimonials.tsx
@@ -2,10 +2,10 @@ import { InfiniteMovingCards } from "@/components/infinite-moving-cards";
 
 const testimonials = [
   {
-    name: "Steven Tey",
+    name: "Marc Seitz — oss/acc",
     avatarUrl:
-      "https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg",
-    handle: "@steventey",
+      "https://pbs.twimg.com/profile_images/1176854646343852032/iYnUXJ-m_400x400.jpg",
+    handle: "@mfts0",
     verified: true,
     quote:
       "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
@@ -38,34 +38,38 @@ const testimonials = [
       "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
   },
   {
-    name: "Steven Tey",
+    name: "félix malfait",
+    verified: true,
     avatarUrl:
-      "https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg",
-    handle: "@steventey",
+      "https://pbs.twimg.com/profile_images/1248996603458961409/Dh5rQ44D_400x400.jpg",
+    handle: "@_felx",
     quote:
       "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
   },
   {
-    name: "Steven Tey",
+    name: "Stevie Graham",
+    verified: true,
     avatarUrl:
-      "https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg",
-    handle: "@steventey",
+      "https://pbs.twimg.com/profile_images/1246203840484929536/mGC2lxSa_400x400.jpg",
+    handle: "@stevegraham",
+    quote:
+      "Nothing more personally gratifying for me than seeing a beautifully designed app using @teller. Get yourselves on the @middayai waiting list now!",
+  },
+  {
+    name: "Zeno Rocha",
+    avatarUrl:
+      "https://pbs.twimg.com/profile_images/1145166093029265408/9gJSVrQ7_400x400.jpg",
+    handle: "@zenorocha",
+    verified: true,
     quote:
       "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
   },
   {
-    name: "Steven Tey",
+    name: "Timur Ercan",
     avatarUrl:
-      "https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg",
-    handle: "@steventey",
-    quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-  },
-  {
-    name: "Steven Tey",
-    avatarUrl:
-      "https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg",
-    handle: "@steventey",
+      "https://pbs.twimg.com/profile_images/1382036502390181888/4BT30oTM_400x400.jpg",
+    handle: "@ElTimuro",
+    verified: true,
     quote:
       "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
   },
