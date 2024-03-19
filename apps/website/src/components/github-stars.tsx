728c77180a8bc8713f11d911080d2b8290d3973f
Pontus Abrahamsson <pontus@lostisland.co>
2024-03-19 15:58:00 +0100

    Add stats

--- apps/website/src/components/github-stars.tsx
+++ apps/website/src/components/github-stars.tsx
@@ -13,7 +13,7 @@ export async function GithubStars() {
   return (
     <a
       href="https://git.new/midday"
-      className="border border-border flex justify-center h-8 leading-[30px] text-[#707070] dark:text-[#878787]"
+      className="border border-border flex justify-center h-8 leading-[30px] text-[#707070] dark:text-[#878787] mr-6 md:mr-0"
     >
       <div className="bg-[#F2F1EF] dark:bg-[#1D1D1D] pl-2 pr-3 text-[14px] flex items-center space-x-2 border-r-[1px] border-border">
         <svg
