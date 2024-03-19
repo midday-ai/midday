c4287a6ca570a07fc4f3aeb8108774d7626557ca
Pontus Abrahamsson <pontus@lostisland.co>
2024-03-19 21:59:20 +0100

    Update assets

--- packages/ui/src/components/navigation-menu.tsx
+++ packages/ui/src/components/navigation-menu.tsx
@@ -54,7 +54,7 @@ const NavigationMenuTrigger = React.forwardRef<
   >
     {children}{" "}
     <ChevronDownIcon
-      className="relative top-[1px] ml-1 h-3 w-3 transition duration-300 group-data-[state=open]:rotate-180"
+      className="relative top-0 ml-1 h-3 w-3 transition duration-300 group-data-[state=open]:rotate-180"
       aria-hidden="true"
     />
   </NavigationMenuPrimitive.Trigger>
