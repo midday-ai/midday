3d538654dd77cf0ba62de3e7d0b1a160dd2d5e38
Pontus Abrahamsson <pontus@lostisland.co>
2024-03-19 19:18:15 +0100

    Fix copy

--- apps/website/src/app/[locale]/story/page.tsx
+++ apps/website/src/app/[locale]/story/page.tsx
@@ -50,8 +50,7 @@ export default function Page({
         all-in-one tools, we embarked on developing an all-in-one business OS.
         Our goal is to help entrepreneurs gain deeper business insights,
         streamline tedious tasks, and serve as a bridge between you and your
-        accountant, allowing you to focus on the enjoyable aspects of your
-        work."
+        accountant, allowing you to focus on the enjoyable aspects of your work.
       </p>
 
       <h3 className="font-medium text-xl mb-2">Open source</h3>
@@ -60,7 +59,7 @@ export default function Page({
         collaboration with users to build the best possible product. Whether
         it's through 15-minute user calls, building in public, or open-sourcing
         our system, these are values we hold dear and will continue to uphold,
-        regardless of how far or big we go."
+        regardless of how far or big we go.
       </p>
 
       <Image src={founders} width={800} height={514} alt="Pontus & Viktor" />
