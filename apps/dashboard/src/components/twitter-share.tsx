3e9cfe6c46a27a3952e684aff8f50201d57a34d7
Pontus Abrahamsson <pontus@lostisland.co>
2024-03-21 16:47:23 +0100

    Add more tweets

--- apps/dashboard/src/components/twitter-share.tsx
+++ apps/dashboard/src/components/twitter-share.tsx
@@ -43,12 +43,25 @@ const popupCenter = ({ url, title, w, h }) => {
   return newWindow;
 };
 
+function chooseRandomMessage() {
+  const messages = [
+    "I just signed up for @middayai, excited to try this out! https://midday.ai",
+    "Just registered for @middayai - because I want to run my business smarter. https://midday.ai",
+    `Ready to elevate my business game with @middayai! Just signed up because it's time to work smarter, not harder. https://midday.ai`,
+    "Taking the first step towards smarter business decisions by joining @middayai. https://midday.ai",
+    "Registered for @middayai to ensure my business stays ahead of the curve. https://midday.ai",
+  ];
+
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  return messages[randomIndex];
+}
+
 export function TwitterShare() {
   const supabase = createClient();
+  const [isLoading, setLoading] = useState(false);
   const approvedUser = useAction(approveUserAction, {
     onSuccess: () => setLoading(false),
   });
-  const [isLoading, setLoading] = useState(false);
 
   const onShare = async () => {
     const {
@@ -58,7 +71,7 @@ export function TwitterShare() {
     setLoading(true);
 
     const popup = popupCenter({
-      url: "https://twitter.com/intent/tweet?text=I just signed up for @middayai, excited to try this out! https://midday.ai",
+      url: `https://twitter.com/intent/tweet?text=${chooseRandomMessage()}`,
       title: "Share",
       w: 800,
       h: 400,
