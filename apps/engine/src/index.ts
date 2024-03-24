00dfa67496a0461d29f70b724413ab740ee2921d
Pontus Abrahamsson <pontus@lostisland.co>
2024-03-24 16:03:34 +0100

    Add cache

--- apps/engine/src/index.ts
+++ apps/engine/src/index.ts
@@ -1,6 +1,7 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { env } from "hono/adapter";
 import { bearerAuth } from "hono/bearer-auth";
+import { cache } from "hono/cache";
 import accounts from "./routes/accounts";
 import institutions from "./routes/institutions";
 import transactions from "./routes/transactions";
@@ -19,11 +20,18 @@ const app = new OpenAPIHono({
   },
 });
 
-app.use("/*", (c, next) => {
-  const { SECRET_KEY } = env<{ SECRET_KEY: string }>(c);
-  const bearer = bearerAuth({ token: SECRET_KEY });
-  return bearer(c, next);
-});
+app.use(
+  "/*",
+  (c, next) => {
+    const { SECRET_KEY } = env<{ SECRET_KEY: string }>(c);
+    const bearer = bearerAuth({ token: SECRET_KEY });
+    return bearer(c, next);
+  },
+  cache({
+    cacheName: "engine",
+    cacheControl: "max-age=3600",
+  })
+);
 
 app.route("/v1/transactions", transactions);
 app.route("/v1/accounts", accounts);
