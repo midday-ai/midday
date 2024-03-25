0b3ccc82c4796117118bd13f1f073dd612df1604
Pontus Abrahamsson <pontus@lostisland.co>
2024-03-25 07:57:55 +0100

    Add 2fa code

--- apps/dashboard/src/components/enroll-mfa.tsx
+++ apps/dashboard/src/components/enroll-mfa.tsx
@@ -1,9 +1,16 @@
 import { createClient } from "@midday/supabase/client";
 import { Button } from "@midday/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@midday/ui/collapsible";
 import { InputOTP, InputOTPGroup, InputOTPSlot } from "@midday/ui/input-otp";
+import { CaretSortIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { CopyInput } from "./copy-input";
 
 export function EnrollMFA() {
   const supabase = createClient();
@@ -11,7 +18,9 @@ export function EnrollMFA() {
   const [isValidating, setValidating] = useState(false);
   const [factorId, setFactorId] = useState("");
   const [qr, setQR] = useState("");
+  const [secret, setSecret] = useState();
   const [error, setError] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const onComplete = async (code: string) => {
     setError(false);
@@ -47,6 +56,7 @@ export function EnrollMFA() {
       setFactorId(data.id);
 
       setQR(data.totp.qr_code);
+      setSecret(data.totp.secret);
     }
 
     enroll();
@@ -63,9 +73,9 @@ export function EnrollMFA() {
   return (
     <>
       <div className="flex items-center justify-center">
-        <div className="w-[190px] h-[190px] bg-white rounded-md">
+        <div className="w-[220px] h-[220px] bg-white rounded-md">
           {qr && (
-            <Image src={qr} alt="qr" width={190} height={190} quality={100} />
+            <Image src={qr} alt="qr" width={220} height={220} quality={100} />
           )}
         </div>
       </div>
@@ -75,6 +85,22 @@ export function EnrollMFA() {
           the code to complete the setup.
         </p>
       </div>
+
+      <Collapsible
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        className="w-full mb-4"
+      >
+        <CollapsibleTrigger className="p-0 text-sm w-full">
+          <div className="flex items-center justify-between">
+            <span className="font-medium">Use setup key</span>
+            <CaretSortIcon className="h-4 w-4" />
+          </div>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          {secret && <CopyInput value={secret} className="w-full" />}
+        </CollapsibleContent>
+      </Collapsible>
 
       <div className="flex w-full">
         <InputOTP
