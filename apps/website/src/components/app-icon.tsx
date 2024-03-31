bbff763429aa2e470bff613f9d350dc89cbabb95
Pontus Abrahamsson <pontus@lostisland.co>
2024-03-31 19:49:15 +0200

    Add spline

--- apps/website/src/components/app-icon.tsx
+++ apps/website/src/components/app-icon.tsx
@@ -5,7 +5,6 @@ import Spline from "@splinetool/react-spline";
 export function AppIcon() {
   return (
     <div className="w-[80px] h-[80px] md:mt-0 md:h-auto md:w-auto">
-      {" "}
       <Spline scene="https://prod.spline.design/I6RvH10aCHKh8p-O/scene.splinecode" />
     </div>
   );
