708ba07272621a45d6d5aca90242dd5c926d64d4
Pontus Abrahamsson <pontus@lostisland.co>
2024-04-02 17:43:57 +0200

    Update copy

--- apps/website/src/components/pitch/section-traction.tsx
+++ apps/website/src/components/pitch/section-traction.tsx
@@ -63,9 +63,7 @@ export function SectionTraction() {
               <h2 className="text-2xl">GitHub stars</h2>
 
               <p className="text-[#878787] text-sm text-center">
-                Since going open source on the 20th of March 2024 we’ve gained
-                834 stars on GitHub. Our goal is to build a great community
-                around Midday.
+                Our goal is to build a great community around Midday.
               </p>
 
               <div className="flex items-center space-x-4">
