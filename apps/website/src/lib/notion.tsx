7f0f347ba9519047b11edde43aec7f8220d18aac
Pontus Abrahamsson <pontus@lostisland.co>
2024-04-05 13:03:31 +0200

    Remove loader

--- apps/website/src/lib/notion.tsx
+++ apps/website/src/lib/notion.tsx
@@ -1,10 +1,13 @@
 import "server-only";
 
+import { createBlockRenderer } from "@notion-render/client";
 import { Client } from "@notionhq/client";
 import type {
   BlockObjectResponse,
+  ImageBlockObjectResponse,
   PageObjectResponse,
 } from "@notionhq/client/build/src/api-endpoints";
+import Image from "next/image";
 
 export const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -54,3 +57,28 @@ export const fetchPageBlocks = (pageId: string) => {
     .list({ block_id: pageId })
     .then((res) => res.results as BlockObjectResponse[]);
 };
+
+export const imageRenderer = createBlockRenderer<ImageBlockObjectResponse>(
+  "image",
+  async (data, renderer) => {
+    const src =
+      "file" in data.image ? data.image.file.url : data.image.external.url;
+
+    return `<Image src="${src}" height="400" alt="Image" />`;
+
+    // return <Image  height={400} />;
+
+    // return `
+    //         <figure class="notion-${data.type}">
+    //             <img src="${src}" />
+    //             ${
+    //               data.image.caption.length > 0
+    //                 ? `<legend>${await renderer.render(
+    //                     ...data.image.caption
+    //                   )}</legend>`
+    //                 : ""
+    //             }
+    //         </figure>
+    //     `;
+  }
+);
