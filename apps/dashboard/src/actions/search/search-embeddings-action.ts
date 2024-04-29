c3dd92b697ac66d2e176ed7edccff93334e7a4b5
Pontus Abrahamsson <pontus@lostisland.co>
2024-04-29 09:47:20 +0200

    Debug

--- apps/dashboard/src/actions/search/search-embeddings-action.ts
+++ apps/dashboard/src/actions/search/search-embeddings-action.ts
@@ -10,27 +10,27 @@ export const searchEmbeddingsAction = action(
     const user = await getUser();
 
     const { query, type, limit = 10, threshold } = params;
+    console.log({ query, type, limit });
+    // const res = await fetch(
+    //   `${process.env.NEXT_PUBLIC_SUPABASE_URL}/functions/v1/search`,
+    //   {
+    //     cache: "no-store",
+    //     method: "POST",
+    //     headers: {
+    //       authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+    //     },
+    //     body: JSON.stringify({
+    //       type,
+    //       limit,
+    //       threshold,
+    //       search: query,
+    //       team_id: user?.data?.team_id,
+    //     }),
+    //   }
+    // );
 
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/functions/v1/search`,
-      {
-        cache: "no-store",
-        method: "POST",
-        headers: {
-          authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
-        },
-        body: JSON.stringify({
-          type,
-          limit,
-          threshold,
-          search: query,
-          team_id: user?.data?.team_id,
-        }),
-      }
-    );
+    // const { data } = await res.json();
 
-    const { data } = await res.json();
-
-    return data;
+    return [];
   }
 );
