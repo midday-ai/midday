34a28ce0dc2d4fcb75be428f34402086fa97cfc9
Pontus Abrahamsson <pontus@lostisland.co>
2024-05-08 14:47:18 +0200

    wip

--- apps/dashboard/src/components/select-category.tsx
+++ apps/dashboard/src/components/select-category.tsx
@@ -1,67 +1,60 @@
 import { useI18n } from "@/locales/client";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@midday/ui/select";
-import { Skeleton } from "@midday/ui/skeleton";
+import { Combobox } from "@midday/ui/combobox";
 import { useEffect, useState } from "react";
 import { CategoryIcon, categories } from "./category";
 
-export function SelectCategory({
-  selectedId,
-  isLoading,
-  placeholder,
-  onChange,
-}) {
-  const [value, setValue] = useState();
+type Props = {
+  selectedId: string;
+  isLoading: boolean;
+  placeholder: string;
+  onChange: (selectedId: string) => void;
+};
+
+export function SelectCategory({ selectedId, placeholder, onChange }: Props) {
+  const [value, setValue] = useState("");
+  const [query, setQuery] = useState();
+  const [isLoading, setLoading] = useState(false);
   const t = useI18n();
 
   useEffect(() => {
     setValue(selectedId);
   }, [selectedId]);
 
-  const sortedCategories = Object.values(categories).sort((a, b) => {
-    if (a === value) {
-      return -1;
-    }
+  // const sortedCategories = Object.values(categories).sort((a, b) => {
+  //   if (a === value) {
+  //     return -1;
+  //   }
 
-    if (b === value) {
-      return 1;
-    }
+  //   if (b === value) {
+  //     return 1;
+  //   }
 
-    return a.localeCompare(b);
-  });
+  //   return a.localeCompare(b);
+  // });
+
+  const defaultCategories = Object.keys(categories).map((category) => ({
+    id: category,
+    name: t(`categories.${category}`),
+  }));
+
+  const options = [...defaultCategories];
+
+  const onSelect = () => {};
 
   return (
-    <div className="relative w-full">
-      {isLoading ? (
-        <div className="h-[36px] border rounded-md">
-          <Skeleton className="h-[14px] w-[40%] rounded-sm absolute left-3 top-[39px]" />
-        </div>
-      ) : (
-        <Select value={value} onValueChange={onChange}>
-          <SelectTrigger
-            id="category"
-            className="line-clamp-1 truncate"
-            onKeyDown={(evt) => evt.preventDefault()}
-          >
-            <SelectValue placeholder={placeholder} />
-          </SelectTrigger>
-          <SelectContent className="overflow-y-auto max-h-[350px]">
-            {sortedCategories.map((category) => (
-              <SelectItem key={category} value={category}>
-                <div className="flex space-x-2 items-center">
-                  <CategoryIcon name={category} />
-                  <span>{t(`categories.${category}`)}</span>
-                </div>
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      )}
-    </div>
+    <Combobox
+      showIcon={false}
+      className="border border-border rounded-md p-2"
+      placeholder={placeholder}
+      value={value}
+      onValueChange={(query) => {
+        setLoading(true);
+        setQuery(query);
+      }}
+      onSelect={onSelect}
+      options={options}
+      isLoading={isLoading}
+      classNameList="mt-2"
+    />
   );
 }
