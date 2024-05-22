ca95d943d0491e4a1eaf4897b14be359957836a5
Pontus Abrahamsson <pontus@lostisland.co>
2024-05-22 11:37:14 +0200

    Copy

--- apps/dashboard/src/components/system-banner.tsx
+++ apps/dashboard/src/components/system-banner.tsx
@@ -3,7 +3,7 @@ export function SystemBanner() {
     <div className="p-1 fixed left-0 right-0 top-0 bg-[#FFD02B] z-50 text-center flex items-center justify-center text-black text-sm font-medium">
       <span>
         We are currently experiencing some issues. For the latest updates, you
-        can go go{" "}
+        can go{" "}
         <a href="https://status.midday.ai" className="underline">
           here
         </a>
