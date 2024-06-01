143bf894ada2a8cfcdb348da80df7304d07393f7
Pontus Abrahamsson <pontus@lostisland.co>
2024-06-01 12:44:34 +0200

    Remove url

--- packages/events/src/client.tsx
+++ packages/events/src/client.tsx
@@ -7,7 +7,6 @@ import {
 
 const Provider = () => (
   <OpenpanelProvider
-    url="https://api.openpanel.dev"
     clientId={process.env.NEXT_PUBLIC_OPENPANEL_CLIENT_ID!}
     trackScreenViews={true}
     trackAttributes={true}
