d695ff1959fc71e5c03900d6a74d1c3750eabdee
Pontus Abrahamsson <pontus@lostisland.co>
2024-06-15 14:45:15 +0200

    Remove comment

--- apps/dashboard/src/components/widgets/insights/index.tsx
+++ apps/dashboard/src/components/widgets/insights/index.tsx
@@ -6,11 +6,9 @@ export function Insights() {
     <div className="border aspect-square overflow-hidden relative flex flex-col p-4 md:p-8">
       <h2 className="text-lg">Assistant</h2>
 
-      {/* <div className="flex flex-1 flex-col justify-center items-center"> */}
       <Suspense>
         <InsightsWidget />
       </Suspense>
-      {/* </div> */}
     </div>
   );
 }
