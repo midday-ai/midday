8a99c9bb00e07b45bf143ba6dcf3063fe18508f3
Pontus Abrahamsson <pontus@lostisland.co>
2024-06-30 12:20:25 +0200

    Update hero

--- apps/website/src/components/hero.tsx
+++ apps/website/src/components/hero.tsx
@@ -80,7 +80,7 @@ export function Hero() {
         <p className="text-xs text-[#707070] mt-8 font-mono">
           Used by over{" "}
           <Link href="/open-startup" prefetch>
-            <span className="underline">4100+</span>
+            <span className="underline">4200+</span>
           </Link>{" "}
           businesses.
         </p>
