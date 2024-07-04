f0bdb56c45cd9bd024bc702cca31b989e9bf9e91
Pontus Abrahamsson <pontus@lostisland.co>
2024-07-04 11:54:55 +0200

    Styling

--- apps/dashboard/src/components/header.tsx
+++ apps/dashboard/src/components/header.tsx
@@ -14,7 +14,7 @@ import { MobileMenu } from "./mobile-menu";
 
 export function Header() {
   return (
-    <header className="-ml-4 -mr-4 md:m-0 z-10 px-4 md:pr-0 md:border-b-[1px] flex justify-between pt-4 pb-2 md:pb-4 items-center todesktop:sticky todesktop:top-0 todesktop:bg-background todesktop:border-none sticky md:static top-0 backdrop-filter backdrop-blur-xl md:backdrop-filter md:backdrop-blur-none dark:bg-[#121212] bg-[#fff] bg-opacity-70 ">
+    <header className="-ml-4 -mr-4 md:m-0 z-10 px-4 md:px-0 md:border-b-[1px] flex justify-between pt-4 pb-2 md:pb-4 items-center todesktop:sticky todesktop:top-0 todesktop:bg-background todesktop:border-none sticky md:static top-0 backdrop-filter backdrop-blur-xl md:backdrop-filter md:backdrop-blur-none dark:bg-[#121212] bg-[#fff] bg-opacity-70 ">
       <MobileMenu />
 
       {isDesktopApp() && <DesktopTrafficLight />}
