980bd568f6a5e99713ca5fb0352ba86c5dfc7098
Pontus Abrahamsson <pontus@lostisland.co>
2024-07-04 17:03:58 +0200

    Close modal

--- apps/dashboard/src/components/feedback-form.tsx
+++ apps/dashboard/src/components/feedback-form.tsx
@@ -20,34 +20,45 @@ export function FeedbackForm() {
   return (
     <Popover>
       <PopoverTrigger asChild className="hidden md:block">
-        <Button variant="outline">Feedback</Button>
+        <Button variant="outline" className="rounded-full font-normal">
+          Feedback
+        </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[320px]" sideOffset={10}>
-        <form className="space-y-4">
-          <Textarea
-            name="feedback"
-            value={value}
-            required
-            autoFocus
-            placeholder="Ideas to improve this page or issues you are experiencing."
-            className="resize-none h-[120px]"
-            onChange={(evt) => setValue(evt.target.value)}
-          />
-
-          <div className="mt-1 flex items-center justify-end">
-            <Button
-              type="button"
-              onClick={() => action.execute({ feedback: value })}
-              disabled={value.length === 0 || action.status === "executing"}
-            >
-              {action.status === "executing" ? (
-                <Loader2 className="h-4 w-4 animate-spin" />
-              ) : (
-                "Send"
-              )}
-            </Button>
+      <PopoverContent className="w-[320px] h-[200px]" sideOffset={10}>
+        {action.status === "hasSucceeded" ? (
+          <div className="flex items-center justify-center flex-col space-y-1 mt-10 text-center">
+            <p className="font-medium text-sm">Thank you for your feedback!</p>
+            <p className="text-sm text-[#4C4C4C]">
+              We will be back with you as soon as possible
+            </p>
           </div>
-        </form>
+        ) : (
+          <form className="space-y-4">
+            <Textarea
+              name="feedback"
+              value={value}
+              required
+              autoFocus
+              placeholder="Ideas to improve this page or issues you are experiencing."
+              className="resize-none h-[120px]"
+              onChange={(evt) => setValue(evt.target.value)}
+            />
+
+            <div className="mt-1 flex items-center justify-end">
+              <Button
+                type="button"
+                onClick={() => action.execute({ feedback: value })}
+                disabled={value.length === 0 || action.status === "executing"}
+              >
+                {action.status === "executing" ? (
+                  <Loader2 className="h-4 w-4 animate-spin" />
+                ) : (
+                  "Send"
+                )}
+              </Button>
+            </div>
+          </form>
+        )}
       </PopoverContent>
     </Popover>
   );
