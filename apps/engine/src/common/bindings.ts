a31ef671b42aec6328555ce0028ea0ba9e88d001
Pontus Abrahamsson <pontus@lostisland.co>
2024-07-05 15:08:44 +0200

    Bindings

---
+++ apps/engine/src/common/bindings.ts
@@ -0,0 +1,9 @@
+export type Bindings = {
+  KV: KVNamespace;
+  TELLER_CERT: Fetcher;
+  GOCARDLESS_SECRET_KEY: string;
+  GOCARDLESS_SECRET_ID: string;
+  PLAID_CLIENT_ID: string;
+  PLAID_SECRET: string;
+  PLAID_ENVIRONMENT: string;
+};
