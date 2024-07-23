d9e1ce1ec8e315c2f62b080c481508c97445a41d
Pontus Abrahamsson <pontus@lostisland.co>
2024-07-23 09:20:26 +0200

    Treeshake

--- apps/dashboard/next.config.mjs
+++ apps/dashboard/next.config.mjs
@@ -40,6 +40,16 @@ const config = {
       ".js": [".js", ".ts", ".tsx"],
     };
 
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __SENTRY_DEBUG__: false,
+        __SENTRY_TRACING__: false,
+        __RRWEB_EXCLUDE_IFRAME__: true,
+        __RRWEB_EXCLUDE_SHADOW_DOM__: true,
+        __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
+      }),
+    );
+
     return config;
   },
   async headers() {
