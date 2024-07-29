b428411d50cc4b8cd504b943b895f5fe565326f0
Pontus Abrahamsson <pontus@lostisland.co>
2024-07-29 15:52:17 +0200

    wip

---
+++ packages/email/emails/connection-expire.tsx
@@ -0,0 +1,109 @@
+import {
+  Body,
+  Button,
+  Container,
+  Font,
+  Heading,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+import { addDays, formatDistance } from "date-fns";
+import { Footer } from "../components/footer";
+import { Logo } from "../components/logo";
+
+interface WelcomeProps {
+  fullName: string;
+}
+
+const baseUrl =
+  process.env.VERCEL_ENV === "production"
+    ? "https://midday.ai/email"
+    : "http://localhost:3000/email";
+
+export const ConnectionExpireEmail = ({
+  fullName = "Viktor Hofte",
+  expireAt = addDays(new Date(), 4).toISOString(),
+}: WelcomeProps) => {
+  const firstName = fullName.split(" ").at(0);
+  const text = `Hi ${firstName}, We wanted to let you know about a small issue with the connection to your bank. To keep Midday running smoothly, we'll need you to reconnect your bank account.`;
+
+  return (
+    <Html>
+      <Tailwind>
+        <head>
+          <Font
+            fontFamily="Geist"
+            fallbackFontFamily="Helvetica"
+            webFont={{
+              url: "https://cdn.jsdelivr.net/npm/@fontsource/geist-sans@5.0.1/files/geist-sans-latin-400-normal.woff2",
+              format: "woff2",
+            }}
+            fontWeight={400}
+            fontStyle="normal"
+          />
+
+          <Font
+            fontFamily="Geist"
+            fallbackFontFamily="Helvetica"
+            webFont={{
+              url: "https://cdn.jsdelivr.net/npm/@fontsource/geist-sans@5.0.1/files/geist-sans-latin-500-normal.woff2",
+              format: "woff2",
+            }}
+            fontWeight={500}
+            fontStyle="normal"
+          />
+        </head>
+        <Preview>{text}</Preview>
+
+        <Body className="bg-[#fff] my-auto mx-auto font-sans">
+          <Container
+            className="border-transparent md:border-[#E8E7E1] my-[40px] mx-auto p-[20px] max-w-[600px]"
+            style={{ borderStyle: "solid", borderWidth: 1 }}
+          >
+            <Logo baseUrl={baseUrl} />
+            <Heading className="text-[#121212] text-[21px] font-normal text-center p-0 my-[30px] mx-0">
+              Connection to your bank
+            </Heading>
+
+            <br />
+
+            <span className="font-medium">Hi {firstName},</span>
+            <Text className="text-[#121212]">
+              We hope you're having a great day!
+              <br />
+              <br />
+              We wanted to inform you that our connection to your bank will
+              expire in {formatDistance(new Date(expireAt), new Date())}. To
+              ensure that Midday continues to run smoothly, please reconnect
+              your bank account.
+              <br />
+              <br />
+              The good news? It only takes 60 seconds to get everything back on
+              track!
+            </Text>
+
+            <Section className="text-center mt-[50px] mb-[50px]">
+              <Button
+                className="bg-transparent rounded-md text-primary text-[14px] text-[#121212] font-medium no-underline text-center px-6 py-3 border border-solid border-[#121212]"
+                href="https://go.midday.ai/34Xt7XK"
+              >
+                Reconnect
+              </Button>
+            </Section>
+
+            <br />
+
+            <Footer baseUrl={baseUrl} />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+export default ConnectionExpireEmail;
