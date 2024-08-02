6b9d363772e85172c22b96b0a09e0e99eec13095
Pontus Abrahamsson <pontus@lostisland.co>
2024-08-02 17:14:08 +0200

    wip

--- apps/engine/src/utils/rates.ts
+++ apps/engine/src/utils/rates.ts
@@ -5,15 +5,40 @@ const ENDPOINT =
 
 async function getCurrency(currency: string) {
   const response = await fetch(`${ENDPOINT}/currencies/${currency}.json`);
+
   return response.json();
 }
 
-export async function getRates() {
-  console.log(uniqueCurrencies);
+function transformKeysToUppercase(obj) {
+  // Convert the object into an array of [key, value] pairs
+  const entries = Object.entries(obj);
 
+  // Transform each entry's key to uppercase
+  const upperCaseEntries = entries.map(([key, value]) => {
+    return [key.toUpperCase(), value];
+  });
+
+  // Convert the transformed entries back into an object
+  const transformedObject = Object.fromEntries(upperCaseEntries);
+
+  return transformedObject;
+}
+
+export async function getRates() {
   const rates = await Promise.allSettled(
     uniqueCurrencies.map((currency) => getCurrency(currency.toLowerCase())),
   );
 
-  return rates;
+  return rates
+    .filter((rate) => rate.status === "fulfilled")
+    .map((rate) => rate.value)
+    .map((value) => {
+      const currency = Object.keys(value).at(1);
+
+      return {
+        source: currency?.toUpperCase(),
+        rates: transformKeysToUppercase(value[currency]),
+        // TODO: Filter currencies that are not in the list of unique currencies
+      };
+    });
 }
