c8c540553c904073856283b52f25510a4119e146
Pontus Abrahamsson <pontus@lostisland.co>
2024-08-03 16:22:57 +0200

    Add ticker

--- apps/website/src/components/ticker.tsx
+++ apps/website/src/components/ticker.tsx
@@ -6,33 +6,40 @@ export async function Ticker() {
     admin: true,
   });
 
-  const { data } = await client.rpc(
-    "calculate_total_sum",
-    {
+  const [
+    { data: totalSum },
+    { count: businessCount },
+    { count: transactionCount },
+  ] = await Promise.all([
+    client.rpc("calculate_total_sum", {
       target_currency: "USD",
-    },
-    {
-      get: true,
-    },
-  );
-
-  console.log(data);
+    }),
+    client.from("teams").select("id", { count: "exact", head: true }).limit(1),
+    client
+      .from("transactions")
+      .select("id", { count: "exact", head: true })
+      .limit(1),
+  ]);
 
   return (
-    <div className="text-center flex flex-col mt-[300px] space-y-10">
-      <span className="font-medium text-center text-[100px] md:text-[170px] mb-2 text-stroke leading-none">
-        ${data}
+    <div className="text-center flex flex-col mt-[120px] md:mt-[250px] space-y-4 md:space-y-10">
+      <span className="font-medium text-center text-[55px] md:text-[110px] lg:text-[140px] xl:text-[160px] 2xl:text-[180px] md:mb-2 text-stroke leading-none">
+        ${totalSum}
       </span>
       <span>
-        We proudly help more than{" "}
+        Join over{" "}
         <Link href="/open-startup" className="underline">
-          5,300 businesses
-        </Link>
-        , managing over{" "}
+          {Intl.NumberFormat("en-US", {
+            maximumFractionDigits: 0,
+          }).format(businessCount ?? 0)}
+        </Link>{" "}
+        businesses that rely on us to gain valuable insights from{" "}
         <Link href="/open-startup" className="underline">
-          350,000 transactions
-        </Link>
-        .
+          {Intl.NumberFormat("en-US", {
+            maximumFractionDigits: 0,
+          }).format(transactionCount ?? 0)}
+        </Link>{" "}
+        transactions.
       </span>
     </div>
   );
