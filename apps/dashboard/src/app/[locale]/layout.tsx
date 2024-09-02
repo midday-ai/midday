26aca01cab3bae1be942f3f506c54214b10889f7
Pontus Abrahamsson <pontus@lostisland.co>
2024-09-02 16:49:21 +0200

    Fix lang

--- apps/dashboard/src/app/[locale]/layout.tsx
+++ apps/dashboard/src/app/[locale]/layout.tsx
@@ -1,5 +1,73 @@
+// import { SystemBanner } from "@/components/system-banner";
+import "@/styles/globals.css";
+import { cn } from "@midday/ui/cn";
+import "@midday/ui/globals.css";
+import { Provider as Analytics } from "@midday/events/client";
+import { Toaster } from "@midday/ui/toaster";
+import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
+import type { Metadata } from "next";
 import type { ReactElement } from "react";
 import { Providers } from "./providers";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://app.midday.ai"),
+  title: "Midday | Run your business smarter",
+  description:
+    "Automate financial tasks, stay organized, and make informed decisions effortlessly.",
+  twitter: {
+    title: "Midday | Run your business smarter",
+    description:
+      "Automate financial tasks, stay organized, and make informed decisions effortlessly.",
+    images: [
+      {
+        url: "https://cdn.midday.ai/opengraph-image.jpg",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://cdn.midday.ai/opengraph-image.jpg",
+        width: 1800,
+        height: 1600,
+      },
+    ],
+  },
+  openGraph: {
+    title: "Midday | Run your business smarter",
+    description:
+      "Automate financial tasks, stay organized, and make informed decisions effortlessly.",
+    url: "https://app.midday.ai",
+    siteName: "Midday",
+    images: [
+      {
+        url: "https://cdn.midday.ai/opengraph-image.jpg",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://cdn.midday.ai/opengraph-image.jpg",
+        width: 1800,
+        height: 1600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)" },
+    { media: "(prefers-color-scheme: dark)" },
+  ],
+};
+
+export const preferredRegion = ["fra1", "sfo1", "iad1"];
+export const maxDuration = 60;
 
 export default function Layout({
   children,
@@ -8,5 +76,19 @@ export default function Layout({
   children: ReactElement;
   params: { locale: string };
 }) {
-  return <Providers locale={locale}>{children}</Providers>;
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body
+        className={cn(
+          `${GeistSans.variable} ${GeistMono.variable}`,
+          "whitespace-pre-line overscroll-none",
+        )}
+      >
+        {/* <SystemBanner /> */}
+        <Providers locale={locale}>{children}</Providers>
+        <Toaster />
+        <Analytics />
+      </body>
+    </html>
+  );
 }
