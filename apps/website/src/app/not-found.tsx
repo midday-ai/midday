3fe928e97c94fe9c7a098c4bff96ea76c0523bd0
Pontus Abrahamsson <pontus@lostisland.co>
2024-09-08 12:20:54 +0200

    Fix 404

--- apps/website/src/app/not-found.tsx
+++ apps/website/src/app/not-found.tsx
@@ -2,7 +2,7 @@ import { NotFoundTerminal } from "@/components/not-found-terminal";
 
 export default function NotFound() {
   return (
-    <div className="fixed bg-[#0C0C0C] top-0 right-0 bottom-0 left-0 z-50">
+    <div className="fixed bg-[#0C0C0C] top-0 right-0 bottom-0 left-0 z-30">
       <h1 className="font-mono md:text-[300px] text-[140px] font-medium text-center mt-20">
         404
       </h1>
