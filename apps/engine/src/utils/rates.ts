bbfb34e3b3c12476a5d7f6846f6d5465099b09e9
Pontus Abrahamsson <pontus@lostisland.co>
2024-09-11 14:54:39 +0200

    Update rates

--- apps/engine/src/utils/rates.ts
+++ apps/engine/src/utils/rates.ts
@@ -1,7 +1,7 @@
 import { uniqueCurrencies } from "@midday/location/src/currencies";
 
 const ENDPOINT =
-  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024.8.2/v1";
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1";
 
 async function getCurrency(currency: string) {
   const response = await fetch(`${ENDPOINT}/currencies/${currency}.json`);
