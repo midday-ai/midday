0e76a8c5154946f6e4e3bec44131448d7c4e13ae
yoanyombapro1234 <yoanyombapro@gmail.com>
2024-09-12 20:19:09 -0700

    feat: add ui charts
    Signed-off-by: yoanyombapro1234 <yoanyombapro@gmail.com>

--- packages/ui/.storybook/theme.ts
+++ packages/ui/.storybook/theme.ts
@@ -7,9 +7,8 @@ export default create({
   fontBase: '"Open Sans", sans-serif',
   fontCode: "monospace",
 
-  brandTitle: "Midday | Storybook",
-  brandUrl: "https://midday.ai",
-  brandImage: "https://midday.ai/email/logo-footer-dark.png",
+  brandTitle: "Solomon AI | Storybook",
+  brandUrl: "https://solomon-ai.app",
   brandTarget: "_self",
 
   //
