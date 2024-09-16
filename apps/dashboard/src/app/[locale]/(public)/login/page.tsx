b5dbb3ed232a20b66091f035e38fe7d4502abff0
Lens <45818877+yoanyombapro1234@users.noreply.github.com>
2024-09-16 00:33:08 -0400

    feat: add empty state to platform views
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>

--- apps/dashboard/src/app/[locale]/(public)/login/page.tsx
+++ apps/dashboard/src/app/[locale]/(public)/login/page.tsx
@@ -5,6 +5,7 @@ import { GithubSignIn } from "@/components/github-sign-in";
 import { GoogleSignIn } from "@/components/google-sign-in";
 import { OTPSignIn } from "@/components/otp-sign-in";
 import { SlackSignIn } from "@/components/slack-sign-in";
+import config from "@/config";
 import { Cookies } from "@/utils/constants";
 import { isEU } from "@midday/location";
 import {
@@ -14,16 +15,32 @@ import {
   AccordionTrigger,
 } from "@midday/ui/accordion";
 import { Icons } from "@midday/ui/icons";
+import ClientSection from "@midday/ui/landing/client-section";
+import { FooterSection } from "@midday/ui/landing/footer";
+import HeroSection from "@midday/ui/landing/hero";
+import { SphereMask } from "@midday/ui/magic";
+import Particles from "@midday/ui/magicui/particles";
 import type { Metadata } from "next";
 import { cookies, headers } from "next/headers";
 import Link from "next/link";
 import { userAgent } from "next/server";
+import React from "react";
+
+import { featureFlags } from "@solomon-ai/env/dashboard";
 
 export const metadata: Metadata = {
-  title: "Login | Midday",
+  title: `Login | ${config.company}`,
 };
 
-export default async function Page(params) {
+type AuthProvider = "apple" | "google" | "github" | "slack" | "otp";
+
+type AuthComponentsType = {
+  [key in AuthProvider]: React.ReactElement | null;
+};
+
+export default async function Page(params: {
+  searchParams: { return_to: string };
+}) {
   if (params?.searchParams?.return_to === "desktop/command") {
     return <DesktopCommandMenuSignIn />;
   }
@@ -34,158 +51,132 @@ export default async function Page(params) {
     isEU() && !cookieStore.has(Cookies.TrackingConsent);
   const { device } = userAgent({ headers: headers() });
 
-  let moreSignInOptions = null;
-  let preferredSignInOption =
-    device?.vendor === "Apple" ? (
-      <div className="flex flex-col space-y-2">
-        <GoogleSignIn />
+  // Check which auth providers are enabled
+  const enabledProviders = featureFlags.authProviders;
+  const isAuthEnabled = featureFlags.isAuthEnabled;
+
+  const authComponents: AuthComponentsType = {
+    apple:
+      isAuthEnabled && enabledProviders.includes("apple") ? (
         <AppleSignIn />
-      </div>
-    ) : (
-      <GoogleSignIn />
+      ) : null,
+    google:
+      isAuthEnabled && enabledProviders.includes("google") ? (
+        <GoogleSignIn />
+      ) : null,
+    github:
+      isAuthEnabled && enabledProviders.includes("github") ? (
+        <GithubSignIn />
+      ) : null,
+    slack:
+      isAuthEnabled && enabledProviders.includes("slack") ? (
+        <SlackSignIn />
+      ) : null,
+    otp:
+      isAuthEnabled && enabledProviders.includes("otp") ? <OTPSignIn /> : null,
+  };
+
+  const defaultProvider: AuthProvider =
+    device?.vendor === "Apple" ? "apple" : "google";
+  const preferredProvider =
+    (preferred?.value as AuthProvider) || defaultProvider;
+
+  // Ensure the preferred provider is available, otherwise fall back to the first available provider
+  const availableProviders = Object.keys(authComponents).filter(
+    (key) => authComponents[key as AuthProvider] !== null,
+  ) as AuthProvider[];
+  const actualPreferredProvider = availableProviders.includes(preferredProvider)
+    ? preferredProvider
+    : availableProviders[0];
+
+  let preferredSignInOption =
+    authComponents[actualPreferredProvider as AuthProvider];
+
+  // Filter out the preferred provider and null components
+  let moreSignInOptions = availableProviders
+    .filter((provider) => provider !== actualPreferredProvider)
+    .map((provider) => (
+      <React.Fragment key={provider}>{authComponents[provider]}</React.Fragment>
+    ));
+
+  // If OTP is available and not the preferred option, ensure it's at the end of the list
+  if (authComponents.otp && actualPreferredProvider !== "otp") {
+    moreSignInOptions = moreSignInOptions.filter(
+      (option) => option.key !== "otp",
     );
-
-  switch (preferred?.value) {
-    case "apple":
-      preferredSignInOption = <AppleSignIn />;
-      moreSignInOptions = (
-        <>
-          <GoogleSignIn />
-          <SlackSignIn />
-          <GithubSignIn />
-          <OTPSignIn className="border-t-[1px] border-border pt-8" />
-        </>
-      );
-      break;
-
-    case "slack":
-      preferredSignInOption = <SlackSignIn />;
-      moreSignInOptions = (
-        <>
-          <GoogleSignIn />
-          <AppleSignIn />
-          <GithubSignIn />
-          <OTPSignIn className="border-t-[1px] border-border pt-8" />
-        </>
-      );
-      break;
-
-    case "github":
-      preferredSignInOption = <GithubSignIn />;
-      moreSignInOptions = (
-        <>
-          <GoogleSignIn />
-          <AppleSignIn />
-          <SlackSignIn />
-          <OTPSignIn className="border-t-[1px] border-border pt-8" />
-        </>
-      );
-      break;
-
-    case "google":
-      preferredSignInOption = <GoogleSignIn />;
-      moreSignInOptions = (
-        <>
-          <AppleSignIn />
-          <GithubSignIn />
-          <SlackSignIn />
-          <OTPSignIn className="border-t-[1px] border-border pt-8" />
-        </>
-      );
-      break;
-
-    case "otp":
-      preferredSignInOption = <OTPSignIn />;
-      moreSignInOptions = (
-        <>
-          <GoogleSignIn />
-          <AppleSignIn />
-          <GithubSignIn />
-          <SlackSignIn />
-        </>
-      );
-      break;
-
-    default:
-      if (device?.vendor === "Apple") {
-        moreSignInOptions = (
-          <>
-            <SlackSignIn />
-            <GithubSignIn />
-            <OTPSignIn className="border-t-[1px] border-border pt-8" />
-          </>
-        );
-      } else {
-        moreSignInOptions = (
-          <>
-            <AppleSignIn />
-            <SlackSignIn />
-            <GithubSignIn />
-            <OTPSignIn className="border-t-[1px] border-border pt-8" />
-          </>
-        );
-      }
+    moreSignInOptions.push(
+      <OTPSignIn key="otp" className="border-t-[1px] border-border pt-8" />,
+    );
   }
 
   return (
     <div>
-      <header className="w-full fixed left-0 right-0">
+      <header className="fixed left-0 right-0 w-full">
         <div className="ml-5 mt-4 md:ml-10 md:mt-10">
-          <Link href="https://midday.ai">
+          <Link href={config.webUrl}>
             <Icons.Logo />
           </Link>
         </div>
       </header>
 
-      <div className="flex min-h-screen justify-center items-center overflow-hidden p-6 md:p-0">
-        <div className="relative z-20 m-auto flex w-full max-w-[380px] flex-col py-8">
-          <div className="flex w-full flex-col relative">
-            <div className="pb-4 bg-gradient-to-r from-primary dark:via-primary dark:to-[#848484] to-[#000] inline-block text-transparent bg-clip-text">
-              <h1 className="font-medium pb-1 text-3xl">Login to midday.</h1>
-            </div>
+      <div className="mx-auto flex-1 overflow-hidden">
+        <HeroSection
+          className="md:mt-[250px] mt-[100px]"
+          title={`${config.company} For Business`}
+          subtitle={config.description}
+          ctaText="Get started today with Solomon AI"
+          darkImageSrc=""
+          lightImageSrc=""
+          announcement={`Introducing ${config.company} For Business`}
+        >
+          <div className="pointer-events-auto mb-6 mt-6 flex flex-col items-center">
+            <div className="w-fit">{preferredSignInOption}</div>
 
-            <p className="font-medium pb-1 text-2xl text-[#878787]">
-              Automate financial tasks, <br /> stay organized, and make
-              <br />
-              informed decisions
-              <br /> effortlessly.
-            </p>
-
-            <div className="pointer-events-auto mt-6 flex flex-col mb-6">
-              {preferredSignInOption}
-
-              <Accordion
-                type="single"
-                collapsible
-                className="border-t-[1px] pt-2 mt-6"
-              >
-                <AccordionItem value="item-1" className="border-0">
-                  <AccordionTrigger className="justify-center space-x-2 flex text-sm">
-                    <span>More options</span>
-                  </AccordionTrigger>
-                  <AccordionContent className="mt-4">
-                    <div className="flex flex-col space-y-4">
-                      {moreSignInOptions}
-                    </div>
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </div>
-
+            <Accordion
+              type="single"
+              collapsible
+              className="mt-6 border-t-[1px] pt-2"
+            >
+              <AccordionItem value="item-1" className="border-0">
+                <AccordionTrigger className="flex justify-center space-x-2 text-sm">
+                  <span>More options</span>
+                </AccordionTrigger>
+                <AccordionContent className="mt-4">
+                  <div className="flex flex-col space-y-4">
+                    {moreSignInOptions}
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
             <p className="text-xs text-[#878787]">
               By clicking continue, you acknowledge that you have read and agree
-              to Midday's{" "}
-              <a href="https://midday.ai/terms" className="underline">
+              to {config.name}'s{" "}
+              <a href={`${config.webUrl}/terms`} className="underline">
                 Terms of Service
               </a>{" "}
               and{" "}
-              <a href="https://midday.ai/policy" className="underline">
+              <a href={`${config.webUrl}/policy`} className="underline">
                 Privacy Policy
               </a>
               .
             </p>
           </div>
-        </div>
+        </HeroSection>
+
+        <ClientSection />
+        <SphereMask />
+        <Particles
+          className="absolute inset-0 -z-10"
+          quantity={50}
+          ease={70}
+          size={0.05}
+          staticity={40}
+          color={"#ffffff"}
+        />
+        <FooterSection
+          title={`${config.company} For Business`}
+          description={config.description}
+        />
       </div>
 
       {showTrackingConsent && <ConsentBanner />}
---
+++ apps/dashboard/src/config/index.ts
@@ -0,0 +1,5 @@
+import { BusinessConfig as AppConfig, SiteConfig } from "@internal/app-config";
+
+const config: SiteConfig = AppConfig;
+
+export default config;
