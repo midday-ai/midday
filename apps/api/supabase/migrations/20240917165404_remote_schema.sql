e80621c8968f8d540f77e459fd38b54a95a70a5c
Lens <45818877+yoanyombapro1234@users.noreply.github.com>
2024-09-18 05:17:16 -0400

    feat: wip
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>

--- apps/api/supabase/migrations/20240917165404_remote_schema.sql
+++ apps/api/supabase/migrations/20240917165404_remote_schema.sql
@@ -1,5 +1,3 @@
-CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION handle_new_user();
-
 CREATE TRIGGER user_registered AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION webhook('webhook/registered');
 
 
