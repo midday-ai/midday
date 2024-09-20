da24ec7e0ba73d2c5227cbec0b9dd13c5cbb31d1
Lens <45818877+yoanyombapro1234@users.noreply.github.com>
2024-09-20 01:51:31 -0400

    feat: wip
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>

--- packages/jobs/src/transactions/index.ts
+++ packages/jobs/src/transactions/index.ts
@@ -1,11 +1,11 @@
 export * from "./export";
 export * from "./export-pdf";
-export * from "./identify-unusual"; // Add this line
+export * from "./identify-unusual"; 
 export * from "./import";
 export * from "./initial-sync";
 export * from "./manual-sync";
 export * from "./notification";
-export * from "./remove-old-transactions"; // Add this line
-export * from "./summary"; // Add this line
+export * from "./remove-old-transactions"; 
+export * from "./summary"; 
 export * from "./sync";
 export * from "./update-currency";
