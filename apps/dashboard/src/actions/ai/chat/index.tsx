a28b9aa91a83598bb4ebcba2a2757f58d1ef3d39
Pontus Abrahamsson <pontus@lostisland.co>
2024-09-20 09:05:19 +0200

    Update propmt

--- apps/dashboard/src/actions/ai/chat/index.tsx
+++ apps/dashboard/src/actions/ai/chat/index.tsx
@@ -93,13 +93,13 @@ export async function submitUserMessage(
     system: `\
     You are a helpful assistant in Midday who can help users ask questions about their transactions, revenue, spending find invoices and more.
 
-    If the user just wants the burn rate, call \`getBurnRate\` function.
-    If the user just wants the runway, call \`getRunway\` function.
-    If the user just wants the profit, call \`getProfit\` function.
-    If the user just wants to find transactions and know their expenses, call \`getTransactions\` function.
-    If the user wants to see spending, call \`getSpending\` function.
-    If the user just wants to find invoices or receipts, call \`getInvoices\` function.
-    If the user just wants to find documents, call \`getDocuments\` function.
+    If the user wants the burn rate, call \`getBurnRate\` function.
+    If the user wants the runway, call \`getRunway\` function.
+    If the user wants the profit, call \`getProfit\` function.
+    If the user wants to find transactions or expenses, call \`getTransactions\` function.
+    If the user wants to see spending based on a category, call \`getSpending\` function.
+    If the user wants to find invoices or receipts, call \`getInvoices\` function.
+    If the user wants to find documents, call \`getDocuments\` function.
 
     Always try to call the functions with default values, otherwise ask the user to respond with parameters.
     Current date is: ${new Date().toISOString().split("T")[0]} \n
