b32c60c6dadd11c45b333c9fb86e291cf0d2cc43
Lens <45818877+yoanyombapro1234@users.noreply.github.com>
2024-09-21 06:03:27 -0400

    feat: first commit
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>

--- apps/dashboard/tailwind.config.ts
+++ apps/dashboard/tailwind.config.ts
@@ -1,8 +1,68 @@
-import baseConfig from "@midday/ui/tailwind.config";
+import baseConfig from "@midday/ui/tailwind.config.ts";
 import type { Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{ts,tsx}", "../../packages/ui/src/**/*.{ts,tsx}"],
   presets: [baseConfig],
-  plugins: [require("@todesktop/tailwind-variants")],
+  plugins: [require("@todesktop/tailwind-variants"), require("tailwindcss-animate")],
+  theme: {
+    colors: {
+      "border": "hsl(var(--border))",
+      "input": "hsl(var(--input))",
+      "ring": "hsl(var(--ring))",
+      "background": "hsl(var(--background))",
+      "foreground": "hsl(var(--foreground))",
+      "primary": {
+        "DEFAULT": "hsl(var(--primary))",
+        "foreground": "hsl(var(--primary-foreground))"
+      },
+      "secondary": {
+        "DEFAULT": "hsl(var(--secondary))",
+        "foreground": "hsl(var(--secondary-foreground))"
+      },
+      "destructive": {
+        "DEFAULT": "hsl(var(--destructive))",
+        "foreground": "hsl(var(--destructive-foreground))"
+      },
+      "muted": {
+        "DEFAULT": "hsl(var(--muted))",
+        "foreground": "hsl(var(--muted-foreground))"
+      },
+      "accent": {
+        "DEFAULT": "hsl(var(--accent))",
+        "foreground": "hsl(var(--accent-foreground))"
+      },
+      "popover": {
+        "DEFAULT": "hsl(var(--popover))",
+        "foreground": "hsl(var(--popover-foreground))"
+      },
+      "card": {
+        "DEFAULT": "hsl(var(--card))",
+        "foreground": "hsl(var(--card-foreground))"
+      }
+    },
+    borderRadius: {
+      "lg": "var(--radius)",
+      "md": "calc(var(--radius) - 2px)",
+      "sm": "calc(var(--radius) - 4px)"
+    },
+    keyframes: {
+      "accordion-down": {
+        "from": {
+          "height": "0"
+        },
+        "to": {
+          "height": "var(--radix-accordion-content-height)"
+        }
+      },
+      "accordion-up": {
+        "from": {
+          "height": "var(--radix-accordion-content-height)"
+        },
+        "to": {
+          "height": "0"
+        }
+      }
+    }
+  }
 } satisfies Config;
