00d181eef08d1e6b521a4eeab7bb45298c7323c5
yoanyombapro1234 <yoanyombapro@gmail.com>
2024-09-29 11:42:26 -0400

    feat: fix ci
    Signed-off-by: yoanyombapro1234 <yoanyombapro@gmail.com>

--- .releaserc.js
+++ .releaserc.js
@@ -6,6 +6,21 @@ module.exports = {
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
-    '@semantic-release/git',
+    ['@semantic-release/git', {
+      message: 'chore(release): ${nextRelease.version}'
+    }],
   ],
+  preset: 'angular',
+  releaseRules: [
+    {type: 'docs', scope: 'README', release: 'patch'},
+    {type: 'refactor', release: 'patch'},
+    {type: 'style', release: 'patch'},
+  ],
+  parserOpts: {
+    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+  },
+  // Add this configuration to increase the allowed line length
+  rules: {
+    'body-max-line-length': [2, 'always', 200],
+  },
 };
