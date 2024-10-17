9f8741cb1975834f8fe388c4f14d9ebb63f3c771
yoanyombapro1234 <yoanyombapro@gmail.com>
2024-10-16 20:55:04 -0400

    feat: wip
    Signed-off-by: yoanyombapro1234 <yoanyombapro@gmail.com>

--- services/financial-service/src/constants/constant.ts
+++ services/financial-service/src/constants/constant.ts
@@ -1,6 +1,13 @@
 const constants = {
   /** Paths that are publicly accessible without authentication */
-  PUBLIC_PATHS: ["/", "/openapi", "/health", "/openapi.json", "/doc"],
+  PUBLIC_PATHS: [
+    "/",
+    "/openapi",
+    "/health",
+    "/openapi.json",
+    "/v1/api.users", // the users routes should be public as this should be where users can create accounts, ... etcs
+    "/v1/api.apikeys", // the api keys routes should be public as this should be where users can create api keys
+  ],
 
   /** Cache TTL in seconds */
   CACHE_TTL: 3600,
