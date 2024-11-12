e53f82a08756af7dce7d0a525a532f3d9315eca1
Pontus Abrahamsson <pontus@lostisland.co>
2024-11-12 23:57:16 +0100

    Support legacy format

--- packages/invoice/src/templates/html/components/description.tsx
+++ packages/invoice/src/templates/html/components/description.tsx
@@ -3,5 +3,11 @@ import { EditorContent } from "./editor-content";
 
 export function Description({ content }: { content: string }) {
   const value = isValidJSON(content) ? JSON.parse(content) : null;
+
+  // If the content is not valid JSON, return the content as a string
+  if (!value) {
+    return <div className="font-mono leading-4 text-[11px]">{content}</div>;
+  }
+
   return <EditorContent content={value} />;
 }
--- packages/invoice/src/templates/pdf/components/description.tsx
+++ packages/invoice/src/templates/pdf/components/description.tsx
@@ -1,10 +1,16 @@
-import { View } from "@react-pdf/renderer";
+import { Text, View } from "@react-pdf/renderer";
 import { isValidJSON } from "../../../utils/content";
 import { EditorContent } from "./editor-content";
 
 export function Description({ content }: { content: string }) {
   const value = isValidJSON(content) ? JSON.parse(content) : null;
 
+  // If the content is not valid JSON, return the content as a string
+  if (!value) {
+    return (
+      <Text style={{ fontFamily: "Helvetica", fontSize: 9 }}>{content}</Text>
+    );
+  }
   return (
     <View style={{ flex: 3, alignSelf: "flex-start", marginTop: -10 }}>
       <EditorContent content={value} />
