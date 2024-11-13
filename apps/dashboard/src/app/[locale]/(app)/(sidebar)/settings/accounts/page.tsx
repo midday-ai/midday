8f536ec877356bf008a62dbd36d2cae3a1f1e6c5
Pontus Abrahamsson <pontus@lostisland.co>
2024-11-13 14:01:24 +0100

    Add message

--- apps/dashboard/src/app/[locale]/(app)/(sidebar)/settings/accounts/page.tsx
+++ apps/dashboard/src/app/[locale]/(app)/(sidebar)/settings/accounts/page.tsx
@@ -1,5 +1,6 @@
 import { BaseCurrency } from "@/components/base-currency/base-currency";
 import { ConnectedAccounts } from "@/components/connected-accounts";
+import { Alert, AlertDescription, AlertTitle } from "@midday/ui/alert";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -9,6 +10,15 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <div className="space-y-12">
+      <Alert>
+        <AlertTitle className="mb-1">Temporary Sync Issue</AlertTitle>
+        <AlertDescription className="text-sm text-[#606060]">
+          If you currently experience any issues with missing transactions, try
+          syncing one account at a time by disabling all accounts except one and
+          then pressing sync. We are working on a permanent fix, sorry for the
+          inconvenience.
+        </AlertDescription>
+      </Alert>
       <ConnectedAccounts />
       <BaseCurrency />
     </div>
