d8d94b9aa8501a2b311f3ec2d553984e4520f4ef
Pontus Abrahamsson <pontus@lostisland.co>
2024-11-14 16:40:47 +0100

    log server components

--- apps/dashboard/src/instrumentation.ts
+++ apps/dashboard/src/instrumentation.ts
@@ -1,3 +1,8 @@
+import * as Sentry from "@sentry/nextjs";
+
+// Errors from Nested React Server Components
+export const onRequestError = Sentry.captureRequestError;
+
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("../sentry.server.config");
