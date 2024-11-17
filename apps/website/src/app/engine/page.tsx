33a668bf2161beea63fb8daca8958bd85bb7362b
Pontus Abrahamsson <pontus@lostisland.co>
2024-11-17 16:37:01 +0100

    Fix images

--- apps/website/src/app/engine/page.tsx
+++ apps/website/src/app/engine/page.tsx
@@ -12,8 +12,8 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="w-full bg-[#0C0C0C] flex flex-col items-center justify-center mt-24">
-      <h1 className="text-[100px] md:text-[170px] font-medium text-center text-white relative z-20 leading-none">
+    <div className="w-full dark:bg-[#0C0C0C] flex flex-col items-center justify-center mt-24">
+      <h1 className="text-[100px] md:text-[170px] font-medium text-center text-primary relative z-20 leading-none">
         One API
       </h1>
 
