872b4a15589365754b96f352bb04f770f10600bb
Pontus Abrahamsson <pontus@lostisland.co>
2025-02-16 17:50:13 +0100

    Update metrics.tsx

--- apps/website/src/components/metrics.tsx
+++ apps/website/src/components/metrics.tsx
@@ -6,19 +6,19 @@ export function Metrics() {
       <Link href="/open-startup">
         <div className="flex flex-col md:pr-8 text-center">
           <h4 className="text-[#878787] text-sm mb-4">Businesses</h4>
-          <span className="text-2xl font-mono text-stroke">12,500+</span>
+          <span className="text-2xl font-mono text-stroke">12,700+</span>
         </div>
       </Link>
       <Link href="/open-startup">
         <div className="flex flex-col md:px-8 text-center">
           <h4 className="text-[#878787] text-sm mb-4">Bank accounts</h4>
-          <span className="text-2xl font-mono text-stroke">5,000+</span>
+          <span className="text-2xl font-mono text-stroke">5,200+</span>
         </div>
       </Link>
       <Link href="/open-startup">
         <div className="flex flex-col md:px-8 text-center">
           <h4 className="text-[#878787] text-sm mb-4">Transactions</h4>
-          <span className="text-2xl font-mono text-stroke">1.1M</span>
+          <span className="text-2xl font-mono text-stroke">1.2M</span>
         </div>
       </Link>
       <Link href="/open-startup">
