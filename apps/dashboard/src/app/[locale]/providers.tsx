9709a36bdf346de985d9c18e74d3621994cbd95e
Pontus Abrahamsson <pontus@lostisland.co>
2025-03-23 08:40:34 +0100

    working

--- apps/dashboard/src/app/[locale]/providers.tsx
+++ apps/dashboard/src/app/[locale]/providers.tsx
@@ -2,6 +2,7 @@
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { I18nProviderClient } from "@/locales/client";
+import { TRPCReactProvider } from "@/trpc/client";
 import { isDesktopApp } from "@todesktop/client-core/platform/todesktop";
 import type { ReactNode } from "react";
 
@@ -19,14 +20,16 @@ type ProviderProps = {
 export function Providers({ locale, children }: ProviderProps) {
   return (
     <I18nProviderClient locale={locale}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
+      <TRPCReactProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </TRPCReactProvider>
     </I18nProviderClient>
   );
 }
