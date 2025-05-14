6d5aab07e1b61b1c2c82729ce05018ada6ce6e3f
Pontus Abrahamsson <pontus@lostisland.co>
2025-05-14 15:39:15 +0200

    wip

---
+++ apps/api/src/utils/engine-client.ts
@@ -0,0 +1,8 @@
+import type { AppType } from "@midday/engine";
+import { hc } from "hono/client";
+
+export const engineClient = hc<AppType>(`${process.env.ENGINE_API_URL}/`, {
+  headers: {
+    Authorization: `Bearer ${process.env.MIDDAY_ENGINE_API_KEY}`,
+  },
+});
