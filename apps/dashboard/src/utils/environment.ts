f04b551d5b559d35763d8fd6a5d4036751b7126c
Pontus Abrahamsson <pontus@lostisland.co>
2025-05-18 15:21:06 +0200

    wip

--- apps/dashboard/src/utils/environment.ts
+++ apps/dashboard/src/utils/environment.ts
@@ -3,8 +3,8 @@ export function getUrl() {
     return process.env.NEXT_PUBLIC_URL;
   }
 
-  if (process.env.NEXT_PUBLIC_VERCEL_TARGET_ENV === "preview") {
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  if (process.env.VERCEL_TARGET_ENV === "preview") {
+    return `https://${process.env.VERCEL_URL}`;
   }
 
   return "http://localhost:3001";
