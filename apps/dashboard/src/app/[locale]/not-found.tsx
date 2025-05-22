f97c07aa289bc3274e016bb1e49e347a1ff75fe4
Pontus Abrahamsson <pontus@lostisland.co>
2025-05-22 13:47:41 +0200

    wip

--- apps/dashboard/src/app/[locale]/not-found.tsx
+++ apps/dashboard/src/app/[locale]/not-found.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import appIcon from "../../../public/appicon.png";
+import appIcon from "public/appicon.png";
 
 export default function NotFound() {
   return (
