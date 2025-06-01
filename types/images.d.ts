4dde3a80623ea12848ab900b53299968fea0925e
Pontus Abrahamsson <pontus@lostisland.co>
2025-06-01 16:27:56 +0200

    types

--- types/images.d.ts
+++ types/images.d.ts
@@ -2,3 +2,13 @@ declare module "*.png" {
   const value: string;
   export default value;
 }
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
