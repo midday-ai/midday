88bdd9b239f97599dc25900df100901fe63fea74
Pontus Abrahamsson <pontus@lostisland.co>
2025-06-11 11:34:45 +0200

    wip

--- packages/desktop-client/src/platform-server.ts
+++
@@ -1,7 +0,0 @@
-import { headers } from "next/headers";
-
-export async function isDesktopApp() {
-  const headersList = await headers();
-  const userAgent = headersList.get("user-agent");
-  return userAgent?.includes("Midday Desktop App");
-}
