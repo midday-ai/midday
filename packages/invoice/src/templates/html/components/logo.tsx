76743e449404eed74ca7b410247f61022ae6124e
Pontus Abrahamsson <pontus@lostisland.co>
2025-06-25 18:49:01 +0200

    Fix formatting

--- packages/invoice/src/templates/html/components/logo.tsx
+++ packages/invoice/src/templates/html/components/logo.tsx
@@ -1,19 +1,19 @@
 type Props = {
-	logo: string;
-	customerName: string;
+  logo: string;
+  customerName: string;
 };
 
 export function Logo({ logo, customerName }: Props) {
-	return (
-		<div className="max-w-[300px]">
-			<img
-				src={logo}
-				alt={customerName}
-				style={{
-					height: 80,
-					objectFit: "contain",
-				}}
-			/>
-		</div>
-	);
+  return (
+    <div className="max-w-[300px]">
+      <img
+        src={logo}
+        alt={customerName}
+        style={{
+          height: 80,
+          objectFit: "contain",
+        }}
+      />
+    </div>
+  );
 }
