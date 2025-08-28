1b76a9e9bec3cd7c344acc1bddc58e3797c7357c
Pontus Abrahamsson <pontus@lostisland.co>
2025-08-28 19:46:29 +0200

    Binding (#586)

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -105,4 +105,5 @@ app.route("/", routers);
 export default {
   port: process.env.PORT ? Number.parseInt(process.env.PORT) : 3000,
   fetch: app.fetch,
+  host: "::", // Listen on all interfaces
 };
