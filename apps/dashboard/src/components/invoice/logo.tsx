e1365ea2cfac92066595d2ba34d4099bc9effca5
Pontus Abrahamsson <pontus@lostisland.co>
2025-11-24 18:50:51 +0100

    wip

--- apps/dashboard/src/components/invoice/logo.tsx
+++ apps/dashboard/src/components/invoice/logo.tsx
@@ -53,16 +53,15 @@ export function Logo() {
         {isLoading ? (
           <Skeleton className="w-full h-full" />
         ) : logoUrl ? (
-          <>
+          <div className="max-w-[300px] h-full">
             <img
               src={logoUrl}
               alt="Invoice logo"
-              className="h-full w-auto max-w-none object-contain"
+              className="h-full w-auto object-contain"
             />
             <button
               type="button"
               className="absolute inset-0 bg-black bg-opacity-50 text-white flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity flex-col gap-1"
-              style={{ width: "auto" }}
               onClick={(e) => {
                 e.preventDefault();
                 setValue("template.logoUrl", undefined, {
@@ -75,7 +74,7 @@ export function Logo() {
               <Icons.Clear className="size-4" />
               <span className="text-xs font-medium">Remove</span>
             </button>
-          </>
+          </div>
         ) : (
           <div className="h-[80px] w-[80px] bg-[repeating-linear-gradient(-60deg,#DBDBDB,#DBDBDB_1px,transparent_1px,transparent_5px)] dark:bg-[repeating-linear-gradient(-60deg,#2C2C2C,#2C2C2C_1px,transparent_1px,transparent_5px)]" />
         )}
