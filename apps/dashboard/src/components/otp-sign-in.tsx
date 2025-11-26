d4346b46cddf9987a3ae2c2e40e7df52601f190b
Pontus Abrahamsson <pontus@lostisland.co>
2025-11-26 10:39:46 +0100

    wip

--- apps/dashboard/src/components/otp-sign-in.tsx
+++ apps/dashboard/src/components/otp-sign-in.tsx
@@ -115,7 +115,7 @@ export function OTPSignIn({ className }: Props) {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
         <div className={cn("flex flex-col space-y-4", className)}>
           <FormField
             control={form.control}
