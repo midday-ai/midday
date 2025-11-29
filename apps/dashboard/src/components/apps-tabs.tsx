55f0f5d03ab1f68c0aa552e5627ba64a329e631e
Pontus Abrahamsson <pontus@lostisland.co>
2025-11-29 16:02:36 +0100

    tabs

--- apps/dashboard/src/components/apps-tabs.tsx
+++ apps/dashboard/src/components/apps-tabs.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@midday/ui/cn";
 import { Tabs, TabsList, TabsTrigger } from "@midday/ui/tabs";
 import { useQueryState } from "nuqs";
 
@@ -10,14 +11,30 @@ export function AppsTabs() {
 
   return (
     <Tabs value={currentTab ?? "all"} onValueChange={setTab}>
-      <TabsList className="h-9">
-        <TabsTrigger value="all" className="h-full px-2 py-0 text-xs">
-          All
-        </TabsTrigger>
-        <TabsTrigger value="installed" className="h-full px-2 py-0 text-xs">
-          Installed
-        </TabsTrigger>
-      </TabsList>
+      <div className="relative flex items-stretch bg-[#f7f7f7] dark:bg-[#131313] w-fit">
+        <TabsList className="flex items-stretch h-auto p-0 bg-transparent">
+          <TabsTrigger
+            value="all"
+            className={cn(
+              "group relative flex items-center gap-1.5 px-3 py-1.5 text-[14px] transition-all whitespace-nowrap border border-transparent h-[34px] min-h-[34px]",
+              "text-[#707070] hover:text-black bg-[#f7f7f7] dark:text-[#666666] dark:hover:text-white dark:bg-[#131313] mb-0 relative z-[1]",
+              "data-[state=active]:text-black data-[state=active]:bg-[#e6e6e6] dark:data-[state=active]:text-white dark:data-[state=active]:bg-[#1d1d1d] data-[state=active]:mb-[-1px] data-[state=active]:z-10",
+            )}
+          >
+            All
+          </TabsTrigger>
+          <TabsTrigger
+            value="installed"
+            className={cn(
+              "group relative flex items-center gap-1.5 px-3 py-1.5 text-[14px] transition-all whitespace-nowrap border border-transparent h-[34px] min-h-[34px]",
+              "text-[#707070] hover:text-black bg-[#f7f7f7] dark:text-[#666666] dark:hover:text-white dark:bg-[#131313] mb-0 relative z-[1]",
+              "data-[state=active]:text-black data-[state=active]:bg-[#e6e6e6] dark:data-[state=active]:text-white dark:data-[state=active]:bg-[#1d1d1d] data-[state=active]:mb-[-1px] data-[state=active]:z-10",
+            )}
+          >
+            Installed
+          </TabsTrigger>
+        </TabsList>
+      </div>
     </Tabs>
   );
 }
