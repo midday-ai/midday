1a297d671ec7e9190ad1583b64c8e30502967af1
Pontus Abrahamsson <pontus@lostisland.co>
2025-12-05 17:58:20 +0100

    Delete chat

--- apps/api/src/trpc/routers/chats.ts
+++ apps/api/src/trpc/routers/chats.ts
@@ -5,6 +5,7 @@ import {
   listChatsSchema,
 } from "@api/schemas/chat";
 import { createTRPCRouter, protectedProcedure } from "@api/trpc/init";
+import { TRPCError } from "@trpc/server";
 
 export const chatsRouter = createTRPCRouter({
   list: protectedProcedure
@@ -20,15 +21,37 @@ export const chatsRouter = createTRPCRouter({
     }),
 
   get: protectedProcedure.input(getChatSchema).query(async ({ ctx, input }) => {
+    const scopedUserId = `${ctx.session.user.id}:${ctx.teamId}`;
+
     return memoryProvider.getMessages({
       chatId: input.chatId,
+      userId: scopedUserId,
       limit: 50,
     });
   }),
 
   delete: protectedProcedure
     .input(deleteChatSchema)
-    .mutation(async ({ input }) => {
+    .mutation(async ({ ctx, input }) => {
+      const scopedUserId = `${ctx.session.user.id}:${ctx.teamId}`;
+
+      // Verify ownership first since deleteChat may not support userId parameter
+      // Try to get messages with userId to verify ownership
+      try {
+        await memoryProvider.getMessages({
+          chatId: input.chatId,
+          userId: scopedUserId,
+          limit: 1,
+        });
+      } catch (error) {
+        // If getMessages fails, the chat doesn't belong to the user
+        throw new TRPCError({
+          code: "FORBIDDEN",
+          message: "You do not have permission to delete this chat",
+        });
+      }
+
+      // If getMessages succeeds, the chat belongs to the user, so we can delete it
       return memoryProvider.deleteChat(input.chatId);
     }),
 });
