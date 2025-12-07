d4a988c65da1cdf1acb769f4409b5f54805cce4f
Pontus Abrahamsson <pontus@lostisland.co>
2025-12-07 22:35:45 +0100

    wip

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -21,13 +21,17 @@ const config = {
     "@midday/invoice",
     "@midday/api",
   ],
-  serverExternalPackages: ["@react-pdf/renderer", "pdfjs-dist"],
+  serverExternalPackages: [
+    "@react-pdf/renderer",
+    "pdfjs-dist",
+    "pino",
+    "pino-pretty",
+    "thread-stream",
+  ],
   typescript: {
     ignoreBuildErrors: true,
   },
   devIndicators: false,
-  // Verify invoice editor etc
-  // reactCompiler: true,
   async headers() {
     return [
       {
