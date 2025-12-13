56793fef4cfc8b61803a252499bed6e2905c9dba
Pontus Abrahamsson <pontus@lostisland.co>
2025-12-13 13:01:56 +0100

    wip

--- packages/app-store/src/gmail/config.ts
+++
@@ -1,8 +0,0 @@
-import { baseConfig } from "./config-base";
-
-// Server-safe config without images - used by API
-// Images are excluded to avoid bun trying to execute PNG files when imported in server contexts
-export default {
-  ...baseConfig,
-  images: [] as string[],
-};
