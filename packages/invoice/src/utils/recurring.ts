2ab47a83d88bca8c386c1ab03c2e6b0867858b4e
Pontus Abrahamsson <pontus@lostisland.co>
2026-01-03 17:26:18 +0100

    wip

--- packages/invoice/src/utils/recurring.ts
+++ packages/invoice/src/utils/recurring.ts
@@ -95,7 +95,7 @@ export function formatOrdinal(n: number): string {
 function getOrdinalSuffix(n: number): string {
   const s = ["th", "st", "nd", "rd"];
   const v = n % 100;
-  return s[(v - 20) % 10] || s[v] || s[0];
+  return s[(v - 20) % 10] ?? s[v] ?? s[0] ?? "th";
 }
 
 /**
