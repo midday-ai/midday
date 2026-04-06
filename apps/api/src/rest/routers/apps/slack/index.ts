292f0907aa75367277df6890a56776335d800a7c
Pontus Abrahamsson <pontus@lostisland.co>
2026-04-06 22:31:31 +0200

    wip

--- apps/api/src/rest/routers/apps/slack/index.ts
+++ apps/api/src/rest/routers/apps/slack/index.ts
@@ -1,6 +1,7 @@
 import type { Context } from "@api/rest/types";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { installUrlRouter } from "./install-url";
+import { interactionsRouter } from "./interactions";
 import { oauthCallbackRouter } from "./oauth-callback";
 import { webhookRouter } from "./webhook";
 
@@ -9,5 +10,6 @@ const app = new OpenAPIHono<Context>();
 app.route("/oauth-callback", oauthCallbackRouter);
 app.route("/install-url", installUrlRouter);
 app.route("/webhook", webhookRouter);
+app.route("/interactions", interactionsRouter);
 
 export { app as slackRouter };
